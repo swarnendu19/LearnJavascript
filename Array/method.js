fa760ef041096818e857a11ad46861a9e5a8ed07
Swarnendu Maity <113368538+swarnendu19@users.noreply.github.com>
2023-08-20 16:32:27 +0000

    ArraY-Basics

---
+++ Array/method.js
@@ -0,0 +1,10 @@
+const myArr =[1,2,34,5,6]
+myArr.entries(5)
+console.log(myArr);
+ 
+console.log(myArr);
+//Slice && Splice
+const m=myArr.slice(1,3)
+console.log(m);
+const m1=myArr.splice(1,3)
+console.log(m1);
