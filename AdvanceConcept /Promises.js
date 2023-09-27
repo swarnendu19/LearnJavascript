86708adec8898f7e68bd7528a4143d32500102e4
Swarnendu Maity <113368538+swarnendu19@users.noreply.github.com>
2023-09-27 08:21:06 +0000

    update

--- AdvanceConcept /Promises.js
+++ AdvanceConcept /Promises.js
@@ -10,5 +10,14 @@ promise1.then(()=>{
     console.log("Promise Comsume");
 })
 
+new Promise(()=>{
+setTimeout(()=>{
+    console.log("Async task 2");
+    resolve() 
+},1000)
+}).then(()=>{
+    console.log("Async Part2 resolved");
+})
 
+ 
 
